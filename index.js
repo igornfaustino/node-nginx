56d66d3489c49deb62e743d462e85f3e2ff6ce08
Igor Neves Faustino <igor@nfaustino.com>
2022-12-22 10:14:57 -0300

    feat: add simple express server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hello world")
+const express = require("express")
+
+const app = express()
+const port = process.env.PORT
+
+app.get('/', (req, res) => {
+    res.send('<h1>Full Cycle Rocks!</h1>')
+})
+
+app.listen(port, () => {
+    console.log(`server running`)
+})
